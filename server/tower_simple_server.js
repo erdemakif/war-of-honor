61cdd5ac69f6f50752239761838b3a9120fe63a4
Erkan Gurel <erkangurel@gmail.com>
2012-04-19 00:49:40 +0300

    server serves static client files

--- server/tower_simple_server.js
+++ server/tower_simple_server.js
@@ -3,35 +3,28 @@ var http = require('http')
 	fs = require('fs')
 	static = require('node-static');
 
-/*var decision = ["take my money", "I surrender take my life", "I see dead people"];
-var requests = 0;
-*/
-
 var clientFiles = new static.Server('./client');
 
 var server = http.createServer( function(req, res){
 	req.addListener('end', function () {
-		clientFiles.serve(req, res);
+		clientFiles.serve(req, res); //this will send all files in ./client folder to the client
   	});
 });
 
-server.listen(8000);
+server.listen(8000); //listening port 8000
 
 var socket = sio.listen(server);
 socket.sockets.on('connection', function (s) {
-	console.log("Socket connected: " + s.id);
 
   	s.on('message', function (data) {
-    	
-  	});
-  	s.on('request', function (req) {
-  		console.log("a " + req + " pushed the button.");
+    	console.log(data);
   	});
 
   	s.on("browser_info", function(info){
   		//here what to do with coming client's info
-  		console.log(info);
+  		console.log("Socket info: "+ s.id + " is  a " + info);
   	});
+
 });
 socket.sockets.on('disconnect', function (s){
 	console.log(s);
