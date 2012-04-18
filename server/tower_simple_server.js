b8719e7375fc6d6a67c8675f82aa9527be602a02
Erkan Gurel <erkangurel@gmail.com>
2012-04-18 22:51:26 +0300

    server serves static client files

--- server/tower_simple_server.js
+++ server/tower_simple_server.js
@@ -1,14 +1,18 @@
 var http = require('http')
 	sio = require('socket.io')
+	fs = require('fs')
+	static = require('node-static');
 
-var msg = "hey browser wassup"
-var decision = ["take my money", "I surrender take my life", "I see dead people"];
+/*var decision = ["take my money", "I surrender take my life", "I see dead people"];
 var requests = 0;
+*/
+
+var clientFiles = new static.Server('./client');
 
 var server = http.createServer( function(req, res){
-	console.log(Math.random());
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("it works! :D");
+	req.addListener('end', function () {
+		clientFiles.serve(req, res);
+  	});
 });
 
 server.listen(8000);
@@ -16,22 +20,22 @@ server.listen(8000);
 var socket = sio.listen(server);
 socket.sockets.on('connection', function (s) {
 	console.log("Socket connected: " + s.id);
+
   	s.on('message', function (data) {
-    	console.log(msg + " is changing to " +data);
-    	msg = data;
+    	
   	});
-  	s.on('request', function (req, res) {
-		if(req.request === "your money or your life"){
-			dec = decision[Math.round(Math.random()*2)];
-			console.log("requested: " + req.request + "returned decision: "+ dec);
-			res(dec);
-			requests++;
-		}
+  	s.on('request', function (req) {
+  		console.log("a " + req + " pushed the button.");
   	});
-  	s.emit("message", msg);
+
+  	s.on("browser_info", function(info){
+  		//here what to do with coming client's info
+  		console.log(info);
+  	});
 });
 socket.sockets.on('disconnect', function (s){
 	console.log(s);
 });
 
-setInterval(function(){ console.log("REQUESTS PER SECOND = "+ requests +" req/sec"); requests=0;}, 1000);
+//measurement
+//setInterval(function(){ console.log("REQUESTS PER SECOND = "+ requests +" req/sec"); requests=0;}, 1000);
