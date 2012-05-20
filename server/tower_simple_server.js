78c2e29716fb043ba984870b4ddcef285385549e
Doguhan Gumusoglu <doguhan.gumusoglu@gmail.com>
2012-05-20 21:39:03 +0300

    switch to redis db, hello world

--- server/tower_simple_server.js
+++ server/tower_simple_server.js
@@ -1,7 +1,13 @@
-var http = require('http')
-	sio = require('socket.io')
-	fs = require('fs')
+var http = require('http');
+	sio = require('socket.io');
+	fs = require('fs');
 	static = require('node-static');
+	redis = require('redis');
+
+var db = redis.createClient(); //no port spesified meaning both redis and node working locally
+var dbUSERS = "USERS";
+var dbSESSIONS = "SESSIONS";
+var dbSTATS = "STATS";
 
 var clientFiles = new static.Server('./client');
 
@@ -13,34 +19,53 @@ var server = http.createServer( function(req, res){
 
 server.listen(8000); //listening port 8000
 
-userInfo = {};
-sessions = {};
-
-
 var socket = sio.listen(server);
+socket.set("origins = *:*"); //to solve: Origin null is not allowed by Access-Control-Allow-Origin. |||NO SUCCESS|||
 socket.sockets.on('connection', function (s) {
 
   	s.on('moregold', function(){
-  		if(sessions[s.id]){
+  		//db.hget(dbUSERS, )
+
+  		/*if(sessions[s.id]){
   			if(userInfo[sessions[s.id].username]) userInfo[sessions[s.id].username].gold++;	
-  		}
+  		}*/
   		
   	});
 
 	s.on('login', function (data) {
+		//todo: can be changed with hsetnx (checks before putting if there is a field)
+		db.hget(dbUSERS, data.username, function(err, res){ 
+			console.log("err: "+err + " res: "+ res);
+			if(!res){
+				db.hset(dbUSERS, data.username, 
+					{gold:0, pass:data.pass, activeSessionID:data.id}, 
+					function(err2, set_res){ console.log("err: "+err2 + " res: "+ set_res);});
+			}
+		}); //if new user
+			
+		/*
   		if(!userInfo[data.username])
 	  		userInfo[data.username] = {gold:0};	
   		sessions[s.id] = {sId:s.id, username:data.username, pass:data.pass};
+  		*/
 	});
 
 	s.on("browser_info", function(info){
-		//here what to do with coming client's info
-		console.log("Socket info: "+ s.id + " is  a " + info);
+		//here what to do with coming client's info -- just for stats
+		db.rpush(dbSTATS, info, console.log("client connected: "+ info));
+		//console.log("Socket info: "+ s.id + " is  a " + info);
 	});
 
 });
 
-setInterval( function(){socket.sockets.emit("gold", userInfo)} , 1000);
+function broadcastGolds(){
+	//can be improved too complex
+
+
+	//console.log("golds sent!");
+}
+
+setInterval( broadcastGolds, 1000);
 
 socket.sockets.on('disconnect', function (s){
 	console.log(s);
